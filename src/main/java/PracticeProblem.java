f25d4b7b0af1e583291b783ce4e376584075955c
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-03-03 02:30:28 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,56 @@
+import java.util.scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	} 
+	public static String pluralize(String word){
+		if (word.endsWith("y")){
+			return "ies";
+		}
+
+		else if(word.endsWith("ey")){
+			return "eys";
+		}
+
+		else if (words.endsWith("ife")){
+			return "ives";
+		}
+		else{
+			return "s";
+		}
+
 	}
 
-}
+	public static int min(int a, int b, int c){
+		if (a <= b && a <= c) {
+            return a;
+        } 
+		else if (b <= a && b <= c) {
+            return b;
+        } 
+		else {
+            return c;
+		}
+	}
+
+	public static boolean isLeapYear(int leap){
+		if (leap % 4 == 0){
+			if (leap % 100 == 0){
+				if (leap % 400 == 0){
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+			else{
+				return true;
+			}
+		}
+		else{
+			return false;
+		}
+	
+		}
+	}
